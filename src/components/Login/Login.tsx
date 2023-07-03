5c5ff2baf309be70c00ee6ecb414f36bc8d4a407
Jatinvaish <1195jatinvaishnav@gmail.com>
2023-07-03 22:59:42 +0530

    issues -- solve styling issues

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import Button from "@mui/material/Button";
 import Dashboard from "@/pages/dashboard/Dashboard";
+import { Box } from "@mui/material";
+import scss from "./SignIN.module.scss";
 
 const Login = () => {
   const { data: session } = useSession();
@@ -16,9 +18,11 @@ const Login = () => {
   }
   return (
     <>
-      <Button variant={"contained"} color={"success"} onClick={() => signIn()}>
-        Sign in
-      </Button>
+      <Box className={scss.center}>
+        <Button variant={"contained"} color={"success"} onClick={() => signIn()}>
+          Sign in
+        </Button>
+      </Box>
     </>
   );
 };
